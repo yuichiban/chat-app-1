103cfb402717a4aa2b9aee8e60140e49493b13f6
yuichiban <bancjapan@gmail.com>
2022-04-11 19:23:01 +0900

    画像表示の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
 
   def create
     @room = Room.find(params[:room_id])
-    @message = @room.messages.new(message_params)
+    @message = @room.messages.new(message_params)   
     if @message.save
       redirect_to room_messages_path(@room)
     else
